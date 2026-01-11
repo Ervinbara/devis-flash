b7e726d84bdfd0dd32c6225d049a980948ce5524
ervin <ervinbara17@gmail.com>
2026-01-11 17:12:55 +0100

    Feature : Legal mention

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -8,16 +8,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/legal')]
 class LegalController extends AbstractController
 {
-    #[Route('/mentions', name: 'legal_mentions')]
+    #[Route('/mentions-legales', name: 'legal_mentions')]
     public function mentions(): Response
     {
         return $this->render('legal/mentions.html.twig');
     }
 
-    #[Route('/privacy', name: 'legal_privacy')]
+    #[Route('/confidentialite', name: 'legal_privacy')]
     public function privacy(): Response
     {
         return $this->render('legal/privacy.html.twig');
